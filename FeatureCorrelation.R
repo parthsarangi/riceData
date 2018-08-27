3afbc99c554fbf47aec04f98aa2d3d59d5cc2ab8
Unknown <parth.sarangi@gmail.com>
2018-08-27 23:16:43 +0700

    featureCorrelation
    feature correlation

---
+++ FeatureCorrelation.R
@@ -0,0 +1,19 @@
+ricedf <- read.csv("riceData.csv")
+names(ricedf)
+head(ricedf)
+dim(ricedf)
+str(ricedf)
+require(caret)
+df1 <- ricedf[c(11:17)]
+df1 <- lapply(df1, as.numeric)
+df1matrix <- as.matrix(df1)
+correlationMatrix <- cor(df1matrix)
+
+
+df1 = ricedf
+for(n in c(1:ncol(df1))) {
+  print(n)
+  #str(df1[[n]])
+  df1[[n]] <- as.numeric(df1[[n]])
+}
+
