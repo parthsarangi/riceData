11817f035a2a914e803c3cc545a7177344c2b0ed
Parth Sarangi <parth.sar@ascendcorp.com>
2018-10-29 11:59:06 +0700

    gaussia, poission, quasi GLM models and outputs

--- glm/glm.R
+++ glm/glm.R
@@ -29,17 +29,36 @@ dim(df.test)
 
 glm.model.gaussian <- glm(yield_pa_area ~ .,data = df,family = 'gaussian')
 glm.model.poisson <- glm(yield_pa_area ~ .,data = df,family = 'poisson')
+glm.model.quasi <- glm(yield_pa_area ~ .,data = df,family = 'quasi')
+
+# Cannot prepare model Inverse Gaussian
+# Error: no valid set of coefficients has been found: please supply starting values
+# In addition: Warning message:
+#   In sqrt(eta) : NaNs produced
+glm.model.inverse.gaussian <- glm(yield_pa_area ~ .,data = df,family = 'inverse.gaussian')
+
+# Error in Model
+# Error in eval(family$initialize) : y values must be 0 <= y <= 1
+glm.model.binomial <- glm(yield_pa_area ~ .,data = df,family = 'binomial')
+
+
 
 # Value from LM
-predicted = predict.lm(model,newdata = df.test[c(-18)],se.fit = TRUE)
+predicted.gaussian = predict.lm(glm.model.gaussian,newdata = df.test[c(-18)])
+predicted.poission = predict.lm(glm.model.poisson,newdata = df.test[c(-18)])
+predicted.quasi = predict.lm(glm.model.quasi,newdata = df.test[c(-18)])
 
 
 #Check lengths of outputs
-length(predicted)
+length(predicted.gaussian)
+length(predicted.poission)
+length(predicted.quasi)
+
+#
+df.results = data.frame(actual=df.test$yield_pa_area,predicted.gaussian,predicted.poission,predicted.quasi)
+
+str(df.results)
+write.csv(df.results,"glm_outputs.csv")
 
 
-df.results = data.frame(actual=df.test$yield_pa_area,predicted)
 
-write.csv(df.results,"svm_outputs.csv")
-str(predicted)
-summary(model)
