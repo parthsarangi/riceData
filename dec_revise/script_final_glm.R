7d03220819ae37ec27c57ee94aebf5b791826265
Parth Sarangi <parth.sar@ascendcorp.com>
2018-12-02 17:40:48 +0700

    glm

--- dec_revise/script_final_glm.R
+++ dec_revise/script_final_glm.R
@@ -1,5 +1,3 @@
-start.time <- Sys.time()
-
 df = read.csv("riceData.csv")
 
 for (i in c(1:3)) {
@@ -11,12 +9,13 @@ df.nrow <- nrow(df)
 index.text <- sample(df.nrow,size = round(0.3*df.nrow))
 df.test <- df[index.text,]
 
-
+start.time <- Sys.time()
 glm.model.gaussian <- glm(yield_pa_area ~ .,data = df,family = 'gaussian')
+build.end.time <- Sys.time()
+build.time.taken <- build.end.time - start.time
 
-predicted.gaussian = predict.lm(glm.model.gaussian,newdata = df.test[c(-18)])
+predicted.gaussian = predict.lm(glm.model.gaussian,newdata = df.test[c(-11)])
+predict.end.time <- Sys.time()
+predict.time.taken <- predict.end.time - start.time
 
-df.results = data.frame(actual=df.test$yield_pa_area,predicted.gaussian)
-end.time <- Sys.time()
-time.taken <- end.time - start.time
-
+print(paste0(" Time to build model ",build.time.taken,'  Time to predict the data ',predict.time.taken))
