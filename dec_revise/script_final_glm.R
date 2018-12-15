6b5e4c844a2c5c9113ba15b3168102b9fe965f71
Parth Sarangi <parth.sar@ascendcorp.com>
2018-12-15 20:32:44 +0700

    finished dec

--- dec_revise/script_final_glm.R
+++ dec_revise/script_final_glm.R
@@ -1,3 +1,6 @@
+# Wordking directory
+# setwd("~/Documents/R/riceData/dec_revise")
+
 df = read.csv("riceData.csv")
 
 for (i in c(1:3)) {
@@ -9,6 +12,8 @@ df.nrow <- nrow(df)
 index.text <- sample(df.nrow,size = round(0.3*df.nrow))
 df.test <- df[index.text,]
 df.train <- df[-index.text,]
+
+final.dataset <- data.frame(actual=df.test$yield_pa_area)
 
 # ------------------------------------------------------------------------------------
 # GLM modelling
@@ -22,8 +27,7 @@ predicted.gaussian = predict.lm(glm.model.gaussian,newdata = df.test[c(-11)])
 predict.end.time <- Sys.time()
 predict.time.taken.gaussian <- predict.end.time - start.time
 
-final.dataset <- data.frame(actual=df.test$yield_pa_area,predicted.gaussian)
-
+final.dataset <- cbind(final.dataset,predicted.gaussian)
 
 # ------------------------------------------------------------------------------------
 # SVM modelling
@@ -46,10 +50,11 @@ final.dataset <- cbind(final.dataset,predicted.svm)
 library(randomForest)
 start.time <- Sys.time()
 
-model.randomForest <- randomForest(yield_pa_area ~ .,data = df.train)
+model.randomForest <- randomForest(yield_pa_area ~ .,data = df.train[-3])
 build.end.time <- Sys.time()
 build.time.taken.random <- build.end.time - start.time
 
+predicted.random = predict(model.randomForest,df.test)
 predict.end.time <- Sys.time()
 predict.time.taken.random <- predict.end.time - start.time
 
@@ -59,15 +64,53 @@ final.dataset <- cbind(final.dataset,predicted.random)
 # ------------------------------------------------------------------------------------
 # neural network
 
+df1 <- df
+
+df1.factor <- as.data.frame(lapply(df1[c(1,2,3)], as.integer))
+df1.continuous <- as.data.frame(lapply(df1[c(4:11)], as.numeric))
+df2.Year_2532.min <- min(df1.factor$X...year)
+df2.Year_2532 <- df1.factor$X...year - df2.Year_2532.min
+
+df2.Regional_min <- min(df1.factor$reginal_zone)
+df2.Regional <- df1.factor$reginal_zone - df2.Regional_min
+
+df2.Province_min <-  min(df1.factor$province_code)
+df2.Province <- df1.factor$province_code - df2.Province_min
+
+df3 <- data.frame(Year_coded=df2.Year_2532,Region_coded=df2.Regional,Province_coded=df2.Province)
+
+df3 <- cbind(df3,df1.continuous)
+
+n = names(df3)
+f <- as.formula(paste(n[11]," ~ ",paste(n[-11],collapse = " + ")))
+
+set.seed(2)
+indx <- sample(round(nrow(df3)*0.7))
+
+# min max normalization
+normalizeMinMax <- function(x) {
+  return ((x - min(x)) / (max(x) - min(x)))
+}
+
+df3.minmax <- as.data.frame(lapply(df3,normalizeMinMax))
+df4.train <- df3.minmax[indx,]
+df4.test <- df3.minmax[-indx,]
+
+library(neuralnet)
+df4.test <- df3.minmax[-indx,]
+
 start.time <- Sys.time()
+model.neuralNetwork <- neuralnet(f,df4.train,hidden = c(2,10),linear.output = TRUE,stepmax = 1e6)
 
 build.end.time <- Sys.time()
 build.time.taken.neural <- build.end.time - start.time
 
+predicted.neural = compute(model.neuralNetwork,df4.test[-11])
+
 predict.end.time <- Sys.time()
 predict.time.taken.neural <- predict.end.time - start.time
 
-final.dataset <- cbind(final.dataset,predicted.neural)
+final.dataset <- cbind(final.dataset,predicted.neural$net.result)
 
 
 # ------------------------------------------------------------------------------------
---
+++ run_script.sh
@@ -0,0 +1,7 @@
+echo
+echo 'Starting sync'
+echo 'by Parth'
+git fetch
+git pull
+echo
+echo 'Finished'
